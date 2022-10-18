17c360b33b9c76458e2f800a0979fe7b103767b6
William Méndez <wmendezc19@gmail.com>
2022-10-18 12:37:31 -0500

    Create backend project structure and healthcheck endpoint

---
+++ backend/src/app.js
@@ -0,0 +1,18 @@
+import compression from "compression";
+import express from "express";
+//Local imports
+import healthRoutes from './routes/health.router.js';
+import morgan from "morgan";
+
+const app = express();
+
+//Middlewares
+app.use(compression());
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+//Routes
+app.use('/api/health', healthRoutes);
+
+export default app;
