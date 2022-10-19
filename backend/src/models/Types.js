eb704583c05315e1c4b4874a686d62a5bb351bc1
William Méndez <wmendezc19@gmail.com>
2022-10-19 16:40:02 -0500

    Feat/initialize backend (#1)
    * Add Docker-compose file with database container config
    
    * Create backend project structure and healthcheck endpoint
    
    * Connect to database and create models

---
+++ backend/src/models/Types.js
@@ -0,0 +1,15 @@
+import { DataTypes } from "sequelize";
+import {sequelize} from '../database/config.js';
+
+export const Types = sequelize.define('types', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING(140)
+    }
+}, {
+    timestamps: false
+});
