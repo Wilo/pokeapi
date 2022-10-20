262cff3f12c8eddf0b36058702a7adb92227efd5
William Méndez <wmendezc19@gmail.com>
2022-10-20 17:09:55 -0500

    Feat/get types endpoint (#2)
    * Fix: rename health controller file
    
    * Feat: Implement Type endpoints
    
    * Add fixtures folder with types csv file
    
    * Fix: Pokemon's weight and height data type and addunique and not null validations
    
    * Fix: fixtures
    
    * Add validation model for url
    
    * Fix: remove comma

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,9 @@
 import compression from "compression";
 import express from "express";
 //Local imports
-import healthRoutes from './routes/health.router.js';
+import healthRoutes from './routes/health.routes.js';
 import morgan from "morgan";
+import typeRoutes from './routes/types.routes.js';
 
 const app = express();
 
@@ -14,5 +15,6 @@ app.use(express.json());
 
 //Routes
 app.use('/api/health', healthRoutes);
+app.use('/api/type/', typeRoutes);
 
 export default app;
