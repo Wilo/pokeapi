dd283b0c43212390aef5c79a98025bd0582486c2
William Méndez <wmendezc19@gmail.com>
2022-10-23 22:39:21 -0500

    Feat: get all pokemons and pokemon detail endpoints

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ import express from "express";
 //Local imports
 import healthRoutes from './routes/health.routes.js';
 import morgan from "morgan";
+import pokemonsRoutes from './routes/pokemons.routes.js';
 import typeRoutes from './routes/types.routes.js';
 
 const app = express();
@@ -16,5 +17,6 @@ app.use(express.json());
 //Routes
 app.use('/api/health', healthRoutes);
 app.use('/api/type/', typeRoutes);
+app.use('/api/pokemons/', pokemonsRoutes);
 
 export default app;
